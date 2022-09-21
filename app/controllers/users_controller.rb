b3348ce0b3fe5f15ec9f233801fe63711ca12d74
nitin7062 <nitinkoolwal999@gmail.com>
2022-09-21 15:57:48 +0530

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # flash[:success] = "Welcome to the Sample App!"
+      respond_to do |format|
+        format.html { redirect_to user_path(@user) }
+      end
+    else
+      render turbo_stream: turbo_stream.replace("test", partial: "shared/error_messages")
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	validates :name, :email, length: {maximum: 100}, presence: true
+	validates :name, length: {maximum: 100}, presence: true
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
